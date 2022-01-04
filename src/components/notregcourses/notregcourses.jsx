a7f5e98c712897e877a4bfbf696edb2fee347122
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2022-01-04 23:54:55 +0100

    added 2 extra components

---
+++ src/components/notregcourses/notregcourses.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import notRegistered from '../../images/emptyReg.png'
+
+function notregcourses() {
+    return (
+        <div class="w-full h-screen ">
+            <div class="text-center w-full h-1/4 text-tprimary py-7 ">
+                <h2 class="font-bold text-4xl py-4 leading-20">Course Registration</h2>
+                <p class="text-xl font-medium leading-20">Oops you dont have any courses to register</p>
+            </div>
+            <div class="h-3/4 flex flex-col items-center justify-center py-14 gap-6 ">
+                <img src={notRegistered} class="w-5/12 h-3/12" alt="reg"/>
+                <button class="bg-bprimary text-white px-2 py-4 w-6/12 font-semibold rounded-lg">Register Courses</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default notregcourses;
