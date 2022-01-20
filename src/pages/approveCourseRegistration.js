6f72db02723700c1b303a9944a1ee6dc95d18fe7
explorarthecreator <fredadejiemma99@gmail.com>
2022-01-20 03:47:23 +0100

    Created the pages or screens needed for the component

---
+++ src/pages/approveCourseRegistration.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import '../App.css'
+import approve from '../approve'
+import AppCourse from '../components/appCourse'
+
+const approveCourseRegistration = () => {
+    return (
+        <>
+            <section id="approval">
+                <h1>approve courses registeration</h1>
+                <h2>400 level first semester</h2>
+            
+                <div class="table">
+                    <table className='w-full'>
+                        <thead>
+                            <tr class="head">
+                                <th>S/N</th>
+                                <th>Matric Number</th>		
+                                <th>Registered Courses</th>
+                                <th>Total Unit</th>
+                                <th>Approve?</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {approve.map(approval =>(
+                                <AppCourse approval={approval}/>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default approveCourseRegistration
