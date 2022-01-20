1f2fb423f43821f9d419466bfa5169beaa0d2006
explorarthecreator <fredadejiemma99@gmail.com>
2022-01-20 03:43:58 +0100

    Created dummy data to be able to test out the components

---
+++ src/courses.js
@@ -0,0 +1,57 @@
+const courses = [
+    {
+        id : 1,
+        courseTitle : 'Operating System',
+        courseCode : 'CSC 411',
+        courseUnit : 2
+    },
+    {
+        id : 2,
+        courseTitle : 'Software Engineering',
+        courseCode : 'CSC 412',
+        courseUnit : 3
+    },
+    {
+        id : 3,
+        courseTitle : 'Organisation of Programming Languages',
+        courseCode : 'CSC 413',
+        courseUnit : 2
+    },
+    {
+        id : 4,
+        courseTitle : 'Artificial Intelligence',
+        courseCode : 'CSC 414',
+        courseUnit : 2
+    },
+    {
+        id : 5,
+        courseTitle : 'Mobile Application Development',
+        courseCode : 'CSC 415',
+        courseUnit : 2
+    },
+    {
+        id : 6,
+        courseTitle : 'Sequential Programming',
+        courseCode : 'CSC 419',
+        courseUnit : 2
+    },
+    {
+        id : 7,
+        courseTitle : 'Entrepreneurial Development',
+        courseCode : 'ENT 411',
+        courseUnit : 1
+    },
+    {
+        id : 8,
+        courseTitle : 'Project Management',
+        courseCode : 'CSC 417',
+        courseUnit : 2
+    },
+    {
+        id : 9,
+        courseTitle : 'DIstributed and Computing System',
+        courseCode : 'CSC 410',
+        courseUnit : 2
+    }
+]
+export default courses
